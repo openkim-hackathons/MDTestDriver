38376f1d5f32bd38e1c291966ed1e94b299fdd74
thomas-egg <tje3676@nyu.edu>
2024-08-15 17:20:59 -0400

    Add driver and job file for running several tests

--- test_driver/test_driver.py
+++ test_driver/test_driver.py
@@ -1,5 +1,6 @@
 from concurrent.futures import as_completed, ProcessPoolExecutor
 import os
+import argparse
 import shutil
 import subprocess
 from typing import Optional, Tuple
@@ -282,12 +283,25 @@ class HeatCapacity(CrystalGenomeTestDriver):
 
 
 if __name__ == "__main__":
-    model_name = "EAM_Dynamo_ErcolessiAdams_1994_Al__MO_123629422045_005"
+    
+    # Argument parsing
+    parser = argparse.ArgumentParser(description='Pass arguments to the KIM test driver')
+    parser.add_argument('-m', '--model', help='Pass model for test driver to run')
+    parser.add_argument('-s', '--stoichiometry', help='Stoichiometry of structure to test', nargs='+')
+    parser.add_argument('-p', '--prototype', help='Prototype ASE label')
+    args = parser.parse_args()
+
+    # Get arguments
+    model_name = args.model
+    stoich = args.stoichiometry
+    prot = args.prototype
+    
+    # Run test 
     subprocess.run(f"kimitems install {model_name}", shell=True, check=True)
     test_driver = HeatCapacity(model_name)
     list_of_queried_structures = query_crystal_genome_structures(kim_model_name=model_name,
-                                                                 stoichiometric_species=['Al'],
-                                                                 prototype_label='A_cF4_225_a')
+                                                                 stoichiometric_species=stoich,
+                                                                 prototype_label=prot)
     for i, queried_structure in enumerate(list_of_queried_structures):
         test_driver(**queried_structure, temperature_K=293.15, 
                     cell_cauchy_stress_eV_angstrom3=[6.241509074460762e-7, 6.241509074460762e-7, 6.241509074460762e-7, 0.0, 0.0, 0.0], 
